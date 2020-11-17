1d7388aa9b562c943af04bfb2ddb23364ba987f2
Flory Ann Evia <floryann.evia@gmail.com>
2020-11-16 19:31:00 -0600

    Deprecation warnings

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
+  newUnifiedTopology: true,
+  UseCreateIndex: true,
   useFindAndModify: false
 });
 
