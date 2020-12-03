3c7af701b781a6e3ec475e10498f739b1a132449
Flory Ann Evia <floryann.evia@gmail.com>
2020-12-03 09:23:32 -0600

    added routes

--- server.js
+++ server.js
@@ -29,7 +29,10 @@ mongoose.connect(
 
 // routes
 app.use(require("./routes/api.js"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "./public/index.html"));
+});
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App listening on http://localhost:${PORT}`);
 });
